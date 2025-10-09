3d3787904941216877ef968a6d412faaf30dde79
Ai Dj <aidjonthedancefloor@gmail.com>
2025-10-09 11:19:09 -0500

    Refactor to abstract chat model creation/vendor

--- src/main/java/com/aidj/aihub/LlmHello.java
+++ src/main/java/com/aidj/aihub/LlmHello.java
@@ -1,24 +1,12 @@
 package com.aidj.aihub;
 
 import dev.langchain4j.model.chat.ChatModel;
-import dev.langchain4j.model.openai.OpenAiChatModel;
-import io.github.cdimascio.dotenv.Dotenv;
-
-import static dev.langchain4j.model.openai.OpenAiChatModelName.GPT_4_O_MINI;
 
 public class LlmHello {
 
-    public static Dotenv dotenv = Dotenv.load();
-    public static final String OPENAI_API_KEY = dotenv.get("OPENAI_API_KEY");
-
     public static void main(String[] args) {
-        ChatModel model = OpenAiChatModel.builder()
-                .apiKey(OPENAI_API_KEY)
-                .modelName(GPT_4_O_MINI)
-                .build();
-
-        String answer = model.chat("What is an RSL?");
-
+        ChatModel model = LlmFactory.createChatModel();
+        String answer = model.chat("What is an Automatic Activation Device?");
         System.out.println(answer);
     }
 }
